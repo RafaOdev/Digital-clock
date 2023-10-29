462da9f90ef32e6809335eb324d865395bf45cd3
Rafael O souza <132023935+nxrStormx@users.noreply.github.com>
2023-10-29 09:44:25 -0300

    new files

---
+++ js/clock.js
@@ -0,0 +1,25 @@
+let container = document.querySelector('.container');
+        let hour = document.querySelector('.hour');
+        let minute = document.querySelector('.minutes');
+        let second = document.querySelector('.seconds');
+
+        setInterval(() => {
+            let date = new Date();
+            let hours = date.getHours();
+            let minutes = date.getMinutes();
+            let seconds = date.getSeconds();
+    
+
+                 hour.innerHTML = `${hours}`;
+                 minute.innerHTML = `${minutes}`;
+                 second.innerHTML = `${seconds}`;
+            if (hours <= 9 ){
+                hour.innerHTML = `0${hours}`;
+            }
+            if (minutes <= 9){
+                minute.innerHTML = `0${minutes}`;
+            }
+             if (seconds <= 9){
+                second.innerHTML = `0${seconds}`
+            }
+        }, 1000)
