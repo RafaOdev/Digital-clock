02a0676485167c35a0014da05c0ddade00da577f
Rafael O souza <132023935+nxrStormx@users.noreply.github.com>
2023-10-30 11:02:29 -0300

    change

--- js/script.js
+++ js/script.js
@@ -13,13 +13,14 @@ let container = document.querySelector('.container');
                  hour.innerHTML = `${hours}`;
                  minute.innerHTML = `${minutes}`;
                  second.innerHTML = `${seconds}`;
-            if (hours <= 9 ){
-                hour.innerHTML = `0${hours}`;
-            }
-            if (minutes <= 9){
-                minute.innerHTML = `0${minutes}`;
-            }
-             if (seconds <= 9){
-                second.innerHTML = `0${seconds}`
-            }
+            
+                 if (hours <= 9 ){
+                    hour.innerHTML = `0${hours}`;
+                }
+                 else if (minutes <= 9){
+                    minute.innerHTML = `0${minutes}`;
+                }
+                 if (seconds <= 9){
+                    second.innerHTML = `0${seconds}`
+                }
         }, 1000)
